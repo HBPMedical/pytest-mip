b1b6d30038b418139bb22237b81013b6bc8f15d4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-03-07 14:24:03 +0100

    feat: add -rPx option flags to pytest call in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 # This script is used to start the container and run pytest
 
 # Run pytest
-pytest -p no:warnings -p no:cacheprovider -v "$@"
+pytest -p no:warnings -p no:cacheprovider -v -rPx "$@"
