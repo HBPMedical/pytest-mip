84f703ceb12d5774f25c27be59067b58a868b812
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2023-03-27 12:40:35 +0200

    feat: generate junitxml test report

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@
 # This script is used to start the container and run pytest
 
 # Run pytest
-pytest -p no:warnings -p no:cacheprovider -v -rPx --html=report/report.html --self-contained-html --css=pytest_report.css "$@"
+pytest \
+  -p no:warnings \
+  -p no:cacheprovider \
+  -vrPx \
+  --html=report/report.html --self-contained-html --css=pytest_report.css \
+  --junitxml=report/junit-report.xml \
+  "$@"
