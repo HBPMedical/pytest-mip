d871ed86102785923eff62b5be8d55e9f1ec5503
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-04-04 16:22:50 +0200

    style: improve javascript code layout and documentation

--- report/js/reports-parser.js
+++ report/js/reports-parser.js
@@ -1,41 +1,56 @@
-/*
- * JS for parsing our reports.json to render the reports table.
- *
-*/
+/**
+ * @file
+ * This file contains the JavaScript code to parse the reports.json and the
+ * individual JUnit XML report files to render the reports table.
+ * @author Sébastien Tourbier
+ * @license Apache-2.0
+ */
 
-/* Define a function to fetch the JSON data and populate the table */
-function parseJSONReports(url = 'https://hbpmedical.github.io/pytest-mip/reports.json') {
-    fetch(url)
-        .then(reports => reports.json())
-        .then(reports => {
-            const tableBody = document.querySelector('#reportsTable tbody');
+/* Define mapping between MIP federation URL and Name displayed in the report table.*/
+const FEDERATION_URL_NAME_MAPPING = {
+  "test_data[https://hbpmip.link/]": "Public",
+  "test_data[https://qa.hbpmip.link/]": "QA",
+};
 
-            reports.reports.forEach(report => {
-                const row = document.createElement('tr');
-
-                const dateCell = document.createElement('td');
-                dateCell.textContent = report.date;
-                row.appendChild(dateCell);
-
-                const linkCell = document.createElement('td');
-                const link = document.createElement('a');
-                link.textContent = report.link;
-                link.href = report.link;
-                link.target = '_blank';
-                linkCell.appendChild(link);
-                row.appendChild(linkCell);
-
-                const statusCell = document.createElement('td');
-                if (report.status === '0'){
-                    statusCell.innerHTML = '<span style="color: green">&#10004;</span>';
-                }
-                else{
-                    statusCell.innerHTML = '<span style="color: red">&#9747;</span>';
-                }
-                row.appendChild(statusCell);
-
-                tableBody.appendChild(row);
-            });
         })
         .catch(error => console.error(error));
+/** Define a function to fetch the JSON data and populate the table
+ * @param {string} url - The URL of the main reports.json file listing all the reports
+ * @returns {void}
+ */
+function parseJSONReports(
+  url = "https://hbpmedical.github.io/pytest-mip/reports.json"
+) {
+  fetch(url)
+    .then((reports) => reports.json())
+    .then((reports) => {
+      const tableBody = document.querySelector("#reportsTable tbody");
+
+      reports.reports.forEach((report) => {
+        const row = document.createElement("tr");
+
+        const dateCell = document.createElement("td");
+        dateCell.textContent = report.date;
+        row.appendChild(dateCell);
+
+        const linkCell = document.createElement("td");
+        const link = document.createElement("a");
+        link.textContent = report.link;
+        link.href = report.link;
+        link.target = "_blank";
+        linkCell.appendChild(link);
+        row.appendChild(linkCell);
+
+        const statusCell = document.createElement("td");
+        if (report.status === "0") {
+          statusCell.innerHTML = '<span style="color: green">&#10004;</span>';
+        } else {
+          statusCell.innerHTML = '<span style="color: red">&#9747;</span>';
+        }
+        row.appendChild(statusCell);
+
+        tableBody.appendChild(row);
+      });
+    })
+    .catch((error) => console.error(error));
 }
--- report/js/table-sorter.js
+++ report/js/table-sorter.js
@@ -1,60 +1,73 @@
-/*
+/**
+ * @file
  * JS for enabling table sorting by column in the reports table.
- *
-*/
+ * @author Sébastien Tourbier
+ */
 
-/* Sort reports table by column */
-function sortTable(n) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("reportsTable");
-    switching = true;
-    // Set the sorting direction to ascending:
-    dir = "asc";
-    /* Make a loop that will continue until
+/** Sort reports table by column.
+ * @param {number} n - The column number to sort by.
+ * @returns {void}
+ * Code adapted from https://www.w3schools.com/howto/howto_js_sort_table.asp.
+ */
+function sortTableByColumn(n) {
+  let table,
+    rows,
+    switching,
+    i,
+    x,
+    y,
+    shouldSwitch,
+    dir,
+    switchcount = 0;
+  table = document.getElementById("reportsTable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
     no switching has been done: */
-    while (switching) {
-        // Start by saying: no switching is done:
-        switching = false;
-        rows = table.rows;
-        /* Loop through all table rows (except the
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
         first, which contains table headers): */
-        for (i = 1; i < (rows.length - 1); i++) {
-            // Start by saying there should be no switching:
-            shouldSwitch = false;
-            /* Get the two elements you want to compare,
+    for (i = 1; i < rows.length - 1; i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
             one from current row and one from the next: */
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-            /* Check if the two rows should switch place,
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
             based on the direction, asc or desc: */
-            if (dir == "asc") {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                    // If so, mark as a switch and break the loop:
-                    shouldSwitch = true;
-                    break;
-                }
-            }
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
         }
-        if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-            // Each time a switch is done, increase this count by 1:
-            switchcount++;
-        } else {
-            /* If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again. */
-            if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-            }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
         }
+      }
     }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
