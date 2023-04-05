602324e325845a0e4d1d505d85e86a4e41996ef6
sebastientourbier <sebastientourbier@users.noreply.github.com>
2023-04-05 08:31:19 +0000

    deploy: 7cdbaae2c908167d66267f65ef808b58a3c57b42

--- js/reports-parser.js
+++ js/reports-parser.js
@@ -64,23 +64,40 @@ function createReportTableHeader() {
   const dateHeader = document.createElement("th");
   dateHeader.innerHTML = "Date";
   dateHeader.setAttribute("onclick", "sortTableByDate()");
+  dateHeader.setAttribute("rowspan", "2");
   headerRow.appendChild(dateHeader);
   // Append the link to the report to the table head
   const linkHeader = document.createElement("th");
   linkHeader.innerHTML = "Link";
+  linkHeader.setAttribute("rowspan", "2");
   headerRow.appendChild(linkHeader);
   // Append the overall status to the table head
   const statusHeader = document.createElement("th");
-  statusHeader.textContent = "Status";
+  statusHeader.innerHTML = "Overall Status";
+  statusHeader.setAttribute("rowspan", "2");
   headerRow.appendChild(statusHeader);
   // Append the federation name to the table head
+  const federationHeaderTitle = document.createElement("th");
+  federationHeaderTitle.innerHTML = "Federation Status";
+  federationHeaderTitle.setAttribute(
+    "colspan",
+    Object.keys(FEDERATION_URL_NAME_MAPPING).length
+  );
+  headerRow.appendChild(federationHeaderTitle);
+
+  // Append the first header row to the table head
+  tableHead.appendChild(headerRow);
+
+  // Create the header row
+  const secondHeaderRow = document.createElement("tr");
+
   for (const [key, value] of Object.entries(FEDERATION_URL_NAME_MAPPING)) {
     const federationHeader = document.createElement("th");
     federationHeader.innerHTML = value;
-    headerRow.appendChild(federationHeader);
+    secondHeaderRow.appendChild(federationHeader);
   }
   // Append the header row to the table head
-  tableHead.appendChild(headerRow);
+  tableHead.appendChild(secondHeaderRow);
 }
 
 /**
@@ -115,16 +132,28 @@ async function createReportTableRow(report) {
   }
   row.appendChild(statusCell);
   // Parse the JUnit XML report file to show status of each federation
-  const jUnitXMLReport = await Promise.resolve(parseJUnitXML(report.junitxml));
-  for (const [key, value] of Object.entries(FEDERATION_URL_NAME_MAPPING)) {
-    const federationCell = document.createElement("td");
-    const test = jUnitXMLReport.find((test) => test.name === value);
-    if (test.status === "passed") {
-      federationCell.innerHTML = '<span style="color: green">&#10004;</span>';
-    } else {
-      federationCell.innerHTML = '<span style="color: red">&#9747;</span>';
+  if (report.junitxml) {
+    console.log("Read JUnitXML report: ", report.junitxml);
+    const jUnitXMLReport = await Promise.resolve(
+      parseJUnitXML(report.junitxml)
+    );
+    for (const [key, value] of Object.entries(FEDERATION_URL_NAME_MAPPING)) {
+      const federationCell = document.createElement("td");
+      const test = jUnitXMLReport.find((test) => test.name === value);
+      if (test.status === "passed") {
+        federationCell.innerHTML = '<span style="color: green">&#10004;</span>';
+      } else {
+        federationCell.innerHTML = '<span style="color: red">&#9747;</span>';
+      }
+      row.appendChild(federationCell);
     }
-    row.appendChild(federationCell);
+  } else {
+    console.log("No JUnitXML report found for: ", report.link);
+    for (const [key, value] of Object.entries(FEDERATION_URL_NAME_MAPPING)) {
+      const federationCell = document.createElement("td");
+      federationCell.innerHTML = '<span style="color: #999;">n/a</span>';
+      row.appendChild(federationCell);
+    }
   }
   // Append the row to the table body
   tableBody.appendChild(row);
