ff4de9c9ecf9933347c154f5a3eabfde31e8b3a9
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2023-06-07 09:43:15 +0200

    style: clean unused imports and extra blank lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-import project_parameters
 
 
 """Define fixtures that will be shared among all federation tests."""
